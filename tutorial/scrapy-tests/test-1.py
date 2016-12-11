e6b8f6529417e384662432662e3de43ae3699d68
Marek <mpietrewicz@gmail.com>
2016-12-11 14:02:04 +0100

    add scrapy test1

---
+++ tutorial/scrapy-tests/test-1.py
@@ -0,0 +1,32 @@
+from twisted.internet import reactor
+import scrapy
+from scrapy.crawler import CrawlerRunner
+
+
+class MySpider(scrapy.Spider):
+    name = "MySpider"
+
+    def start_requests(self):
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+		
+		for x in xpaths:
+			print response.xpath(x)
+
+
+
+#dane wejsciowe:
+urls = [
+	"http://allegro.pl/6-i-2002-2008-147321?a_text_i%5B14%5D%5B0%5D=140&a_enum%5B16%5D%5B1%5D=1&a_enum%5B16%5D%5B4%5D=4&a_enum%5B13%5D%5B3%5D=3&price_to=12000&a_text_i%5B4%5D%5B1%5D=190000&a_enum%5B128790%5D%5B2%5D=2&a_enum%5B178%5D%5B1%5D=1",
+]
+xpath = "//h2[@class='offer-header']/a | //h2[@class='offer-header']/a/@href | //div[@class='offer-price']"
+xpaths = xpath.split(' | ')
+
+
+runner = CrawlerRunner()
+d = runner.crawl(MySpider, input='inputargument', urls=urls, xpaths=xpaths)
+d.addBoth(lambda _: reactor.stop())
+reactor.run() # the script will block here until the crawling is finished
